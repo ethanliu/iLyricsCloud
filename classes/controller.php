6d624a178e7665e4eaaf19e72fbf8ba5dbc35778
Ethan Liu <ethan@creativecrap.com>
2012-01-16 22:36:42 +0800

    changed: collect either GET or POST query for GA

--- classes/controller.php
+++ classes/controller.php
@@ -20,29 +20,38 @@ class Controller {
 	}
 
 	// google analytics section
-	public googleAnalyticsGetImageUrl() {
+	public function googleAnalyticsGetImageUrl() {
 	    $url = "";
 		if (!defined('GA_PIXEL') || !defined('GA_ACCOUNT')) {
 			return $url;
 		}
 		
-	    $url .= GA_PIXEL . "?";
-	    $url .= "utmac=" . GA_ACCOUNT;
-	    $url .= "&utmn=" . rand(0, 0x7fffffff);
+		$url .= GA_PIXEL . "?";
+		$url .= "utmac=" . GA_ACCOUNT;
+		$url .= "&utmn=" . rand(0, 0x7fffffff);
+
+		$referer = $_SERVER["HTTP_REFERER"];
+		//$query = $_SERVER["QUERY_STRING"];
+		//$path = $_SERVER["REQUEST_URI"];
+		$path = $_SERVER["PHP_SELF"];
+	
+		$param = array();
+		foreach ($_REQUEST as $key => $value) {
+			if (!($key == 'key' || $key == 'time')) {
+				$param[] = $key . '=' . $value;
+			}
+		}
+		$path = $path . '?' . implode('&', $param);
 		
-	    $referer = $_SERVER["HTTP_REFERER"];
-	    $query = $_SERVER["QUERY_STRING"];
-	    $path = $_SERVER["REQUEST_URI"];
-		
-	    if (empty($referer)) {
-	      $referer = "-";
-	    }
-	    $url .= "&utmr=" . urlencode($referer);
-	    if (!empty($path)) {
-	      $url .= "&utmp=" . urlencode($path);
-	    }
-	    $url .= "&guid=ON";
-	    return str_replace("&", "&amp;", $url);		
+		if (empty($referer)) {
+			$referer = "-";
+		}
+		$url .= "&utmr=" . urlencode($referer);
+		if (!empty($path)) {
+			$url .= "&utmp=" . urlencode($path);
+		}
+		$url .= "&guid=ON";
+		return str_replace("&", "&amp;", $url);		
 	}
 
 	// sqlite section
