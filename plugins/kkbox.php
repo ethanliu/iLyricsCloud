92c4bdcded3e6c0b5927e8664b35b956cde42db3
Ethan Liu <ethan@creativecrap.com>
2016-04-14 01:39:16 +0800

    Add kkbox plugin for lyrics and artworks

--- plugins/kkbox.php
+++ plugins/kkbox.php
@@ -9,6 +9,7 @@
 function kkbox_artwork_hook($param) {
 	$query = str_replace(' ', '+', ($param['title'] . ' ' . $param['artist']));
 	$url = 'http://tw.kkbox.com/search.php?&search=song&search_lang=&word=' . $query;
+
 	$html = @file_get_contents($url);
 	$url = phpQuery::newDocument($html)->find('div.song-list > table tbody tr:first td:nth-child(3) a')->attr('href');
 	if (empty($url)) {
@@ -17,6 +18,28 @@ function kkbox_artwork_hook($param) {
 
 	$url = 'http://tw.kkbox.com' . $url;
 	$html = @file_get_contents($url);
-	$url = phpQuery::newDocument($html)->find('img.cover')->attr('src');
+	$url = phpQuery::newDocument($html)->find('.album-pic')->attr('src');
+
 	return $url;
+}
+
+function kkbox_lyrics_hook($param) {
+	$query = urlencode($param['title'] . ' ' . $param['artist']);
+	$url = 'http://tw.kkbox.com/search.php?&search=lyrics&word=' . $query;
+
+	$html = file_get_contents($url);
+	$doc = phpQuery::newDocument($html);
+	// fb($html);
+
+	foreach (pq('.lyrics li') as $item) {
+		$text = strtolower(pq($item)->find('h4')->text());
+		if (strpos($text, strtolower($param['title'])) !== false && strpos($text, strtolower($param['artist'])) !== false) {
+			$doc = pq($item)->find('.full-lyrics');
+			pq('a')->remove();
+			$html = strip_tags($doc->html(), '<br>');
+
+			return $html;
+		}
+	}
+	return '';
 }
